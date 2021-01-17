fea67ec75c362e9ccf36985d25b1dfd0bdd09649
NaelSogles <77537796+NaelSogles@users.noreply.github.com>
2021-01-17 01:20:08 +0100

    Add files via upload

---
+++ admin/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<!-- Meta -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- Title -->
+	<title>Espace d'administration</title>
+	<!-- Links -->
+	<link rel="stylesheet" type="text/css" href="css/master.css">
+  <!-- JavaScript -->
+  <script src="https://kit.fontawesome.com/e139eb0a29.js" crossorigin="anonymous"></script>
+
+</head>
+<body>
+	<center>
+	
+		<div class="login">
+			<h1>Administration</h1>
+			
+			<form method="post" action="" autocomplete="off">
+				
+				<input type="text" name="username" placeholder="Nom d'utilisateur">
+				<br /><br />
+				<input type="password" name="password" placeholder="Mot de passe">
+				<br /><br />
+				<input type="submit" name="login_admin" value="Connexion" class="btn-login">
+
+			</form>
+
+		</div>
+
+
+	</center>
